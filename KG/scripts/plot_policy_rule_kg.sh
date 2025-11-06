b6b9e6cdbb08bab9a5475fdc44f09490a891d3ac
nbbb24 <sxysxysxysxy282828@gmail.com>
2025-11-06 13:28:22 -0500

    update OCR policy

--- KG/scripts/plot_policy_rule_kg.sh
+++ KG/scripts/plot_policy_rule_kg.sh
@@ -3,9 +3,9 @@
 # Generate policy rule knowledge graph
 # python generate_policy_rule_kg.py --sql test1/Policy_CGSURG83/SQL_CGSURG83.txt --data-dict test1/Policy_CGSURG83/Data_dictionary_CGSURG83.json --output-dir test1/Policy_CGSURG83 --plot-path test1/Policy_CGSURG83/policy_rule_kg.png
 
-sql_dir="test2/SQL_L34106.txt"
-data_dict_dir="test2/Data_dictionary_L34106.json"
-output_dir="test2"
+sql_dir="test3/SQL_L34106.txt"
+data_dict_dir="test3/Data_dictionary_L34106.json"
+output_dir="test3"
 
 python generate_policy_rule_kg.py \
 --sql $sql_dir \
