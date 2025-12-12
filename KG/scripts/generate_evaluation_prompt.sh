6c62f2bbce3cd1fabaae31412ab1e24b9a13b5fb
nbbb24 <sxysxysxysxy282828@gmail.com>
2025-12-11 22:20:33 -0500

    update cot prompt for evaluation

--- KG/scripts/generate_evaluation_prompt.sh
+++ KG/scripts/generate_evaluation_prompt.sh
@@ -1,10 +1,10 @@
 
-# PROMPT="prompts/Evaluation/data_dictionary_judge_prompt.txt"
-# PROMPT="prompts/Evaluation/patient_extraction_judge_prompt.txt"
-# PROMPT="prompts/Evaluation/policy_condition_judge_prompt.txt"
+# PROMPT="prompts/Evaluation/data_dictionary_judge_prompt_cot.txt"
+# PROMPT="prompts/Evaluation/patient_extraction_judge_prompt_cot.txt"
+PROMPT="prompts/Evaluation/policy_condition_judge_prompt_cot.txt"
 # PROMPT="prompts/Evaluation/data_dictionary_judge_prompt_zero.txt"
 # PROMPT="prompts/Evaluation/patient_extraction_judge_prompt_zero.txt"
-PROMPT="prompts/Evaluation/policy_condition_judge_prompt_zero.txt"
+# PROMPT="prompts/Evaluation/policy_condition_judge_prompt_zero.txt"
 DOC="Run_Time_Policy/LCD_37360/Policy_LCD_37360.txt"
 DOC_PAT="Run_Time_Patient/Patient_L37360001_Policy_LCD_37360/L37360_Record_001.txt"
 PAT="Run_Time_Patient/Patient_L37360001_Policy_LCD_37360/Patient_data_L37360_001.json"
@@ -18,4 +18,4 @@ python prompt_generator.py \
   --extracted-patient $PAT \
   --extracted-dd $DATA \
   --extracted-policy $COND \
-  --output Evaluation
+  --output Evaluation/LCD_37360/
